6f400e9dad9070cdc8080f163ace8e46758d4fa3
Danendra Evan Wicaksono <evanpplgb@gmail.com>
2025-07-16 17:16:15 +0700

    Create research_app.py

---
+++ research_app.py
@@ -0,0 +1,9 @@
+# research_app.py
+class ResearchApp:
+    def search_papers(self, topic, max_results=5):
+        # Implementasi pencarian paper
+        return {"results": [...]}
+    
+    def research_assistant(self, query):
+        # Implementasi asisten penelitian
+        return {"response": "..."}
